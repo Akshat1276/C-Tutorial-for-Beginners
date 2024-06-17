9e9005e6fe612d4962f446a9a309f80731727ab0
Akshat Yadav <71210501+Akshat1276@users.noreply.github.com>
2024-06-18 00:37:19 +0530

    Create example_1.c

---
+++ example_1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+  int x = 5;
+  int y = 10;
+  int z = x*y;
+  printf("Length of rectangle is : %d\n", x);
+  printf("Width of rectangle is : %d\n", y);
+  printf("Area of rectangle is : %d\n", z);
+  return 0;
+}
