3a9e76b166f10832e21875d3dc809539d3d245ca
Akshat Yadav <71210501+Akshat1276@users.noreply.github.com>
2024-06-18 16:26:10 +0530

    Create else.c

---
+++ else.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+  int a = 9;
+  int b = 10;
+  if(a>b){
+    printf("a is greater than b");
+  } else{
+    printf("a is less than b");
+  }
+}
