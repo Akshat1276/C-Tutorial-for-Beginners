a43b97ba5c828d97ace38c01fc508549bff9173d
Akshat Yadav <71210501+Akshat1276@users.noreply.github.com>
2024-06-18 22:20:25 +0530

    Create switch.c

---
+++ switch.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+  int roll_no = 21;
+  switch(roll_no){
+    case 20:
+    printf("raju present");
+    break;
+    case 21:
+    printf("bobby present");
+    break;
+    case 22:
+    printf("monty present");
+    break;
+  }
+  return 0;
+}
