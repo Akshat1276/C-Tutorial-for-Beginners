2d851b4f50496b21c7d24c3b3fc7900d1f2cecc9
Akshat Yadav <71210501+Akshat1276@users.noreply.github.com>
2024-06-18 18:58:49 +0530

    Create else_if.c

---
+++ else_if.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+  int time = 270;
+  if(time <= 25){
+    printf("Goof 1");
+  } else if(time<30){
+    printf("Goof 2");
+  } else{
+    printf("Goof 3");
+  }
+}
