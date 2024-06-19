e05ef69001d7466acb75155647fc46efdee214fb
Akshat Yadav <71210501+Akshat1276@users.noreply.github.com>
2024-06-19 17:51:35 +0530

    Create Addition_Calculator.c

---
+++ Addition_Calculator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void add(){
+  int x, y;
+  printf("Enter 1st no. ");
+  scanf("%d", &x);
+  printf("Enter 2nd no. ");
+  scanf("%d", &y);
+  int z = x+y;
+  printf("The sum of the 2 numbers is: %i", z);
+}
+int main(){
+  add();
+  return 0;
+}
