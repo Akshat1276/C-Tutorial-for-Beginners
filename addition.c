a0507c7f44c158ee025cbdf23f0de69be207b57c
Akshat Yadav <71210501+Akshat1276@users.noreply.github.com>
2024-06-19 23:13:58 +0530

    Create addition.c

---
+++ addition.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+  int x, y;
+  int total = x+y;
+  printf("Enter 1st no.: ");
+  scanf("%i", &x);
+  printf("Enter 2nd no.: ");
+  scanf("%i", &y);
+  printf("The sum of %i & %i is: %i", x, y, total);
+}
